d01d3ff69d87e083d463eeaba6178c747919c25c
YooDongSeon <inko51366@naver.com>
2022-09-28 20:40:48 +0900

    BinarySearch

--- Algorithm/BinarySearch.java
+++ Algorithm/BinarySearch.java
@@ -26,7 +26,6 @@ public class BinarySearch {
         public static void main(String[] args) {
             int[] arr = {1,2,3,4,5,6,7,8,9,10};
             int t = 9;
-
             System.out.println("idx번호 : " + solution(arr, t));       //idx번호 : 8
         }
     }
