c5b93fb8a588d07a63273cc070487528da9f875f
YooDongSeon <inko51366@naver.com>
2022-09-30 22:44:38 +0900

    Linear Queue

--- c++/dataStructure/queue.cpp
+++ c++/dataStructure/queue.cpp
@@ -3,47 +3,47 @@
 
 using namespace std;                              //FIFO
 
-template <typename T>
-
 // push
 // pop
 // size
 // empty
 // front
-// end
+// back
 
+template <typename T>
 
-class Queue{
+class Queue {
     private :
-        T data[MAX_SIZE];
-        int front_idx;
-        int back_idx;
+        int data[MAX_SIZE];
+        int head;                                     //queue의 front를 가리키고있는 포인터
+        int tail;                                     //queue의 tail을 가리키고 있는 포인터
 
     public :
         Queue() {
-            front_idx = -1;
-            back_idx = -1;
+            head = 0;
+            tail = 0;
         }
 
         void push(T input) {
-            this->data[this->back_idx + 1] = input;
+            this->data[this->tail + 1] = input;
+            this->tail++;
         }
 
         void pop() {
-            if(this->empty()) {
-                return;
+            if(!this->empty()) {                      //queue가 비어있지 않으면
+                head++;                               //head를 1증가시킴
             }
             else {
-                
+                return;
             }
         }
 
-        T size() {
-            return this->back_idx - this->front_idx;
+        int size() {
+            return this->tail - this->head;
         }
 
         bool empty() {
-            if(this->size == 0) {
+            if(this->tail - this->head == 0) {
                 return true;
             }
             else {
@@ -52,8 +52,10 @@ class Queue{
         }
 
         T front() {
-            
+            return this->data[head + 1];
         }
 
-        
+        T back() {
+            return this->data[tail];
+        }
 };
